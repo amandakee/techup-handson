4d82ad016a9f9d9b978d5a254f9d992ef22d0880
amandakee <167737379+amandakee@users.noreply.github.com>
2024-05-14 13:13:14 +0000

    completed js handson

---
+++ script.js
@@ -0,0 +1,29 @@
+var age = 20
+age= 65
+var username = "amanda"
+var password = "techup"
+var ismale = false
+var isloggedin = false
+var blog_posts = ["Today was a good day", "The sky is blue", 
+"It's raining heavily now"]
+
+console.log(age)
+console.log (username)
+console.log (password)
+console.log (ismale)
+console.log (isloggedin)
+console.log (blog_posts)
+
+if (ismale == true) {
+    console.log("You are male")
+} else {
+    console.log("You are not male")
+}
+while (age < 70) {
+    console.log ("Your age is " + age)
+    age = age + 1 
+}
+
+function sayHello() {
+    alert("hello everyone");
+}
